55241082bf1914dc545d688f840fca100ae78ea7
splawrence <32889838+splawrence@users.noreply.github.com>
2021-01-14 13:41:56 -0600

    All but node modules

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+      <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+      <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+    <Header/>
+        <h1><strong>Welcome to Best Goat Gifs Ever.biz</strong></h1>
+    <GoatGrid/>
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import GoatGrid from './components/GoatGrid'
+  export default {
+     components:{
+         'Header':Header,
+         'GoatGrid':GoatGrid
+     }
+  }
+</script>
+
+<style>
+  *{
+      box-sizing:border-box;
+      margin:0;
+  }
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+  <div class="jumbotron">
+    <h1>Bootstrap Tutorial</h1>
+    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
+    responsive, mobile-first projects on the web.</p>
+  </div>
+  <p>This is some text.</p>
+  <p>This is another text.</p>
+</div>
+</template>
+
+<script>
+  export default {
+
+  }
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+//   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+//   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</script>
+
+<style>
+
+</style>
